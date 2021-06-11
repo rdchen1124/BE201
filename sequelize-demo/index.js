d7a6cfe8e36be1dfe3fb507a05adbf4c1ac674bd
rdchen1124 <rdchen1124@gmail.com>
2021-06-11 13:56:37 +0800

    'Sequelize 與 ORM' 之 'sequelize CLI - migration 測試 & models 建立關聯級模組測試'

--- sequelize-demo/index.js
+++ sequelize-demo/index.js
@@ -1,49 +1,10 @@
-const { Sequelize } = require('sequelize');
-//mysql_setup
-const dbConfig = {
-    HOST : 'localhost',
-    dialect : 'mysql',
-    DB : 'ryan',
-    USER : 'ryan',
-    PASSWORD : 'ryan'
-}
+const db = require('./models');
+const users = db.users;
+const comments = db.comments;
 
-//create database
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
-    dialect: dbConfig.dialect,
-});
-
-//operate model of sequelize
-const User = sequelize.define('seq_users',{
-    firstName : {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    lastName : {
-        type: Sequelize.STRING,
-    }
-});
-
-//new model for left join
-const Comment = sequelize.define('seq_comments',{
-    content : {
-        type: Sequelize.STRING,
-    }
-});
-// 建立 User 對 Comment 的連結 : 使用者可以有很多評論
-User.hasMany(Comment);
-Comment.belongsTo(User);
-//sequelize.sync().then() > 把上面定義好的資料表(User 對應的那個)產生出來
-//產生完後執行 then() 裡面的 cb()
-sequelize.sync().then(()=>{
-    //do somthing with sequelize
-    Comment.findOne({
-        where : {
-            content : '不繳不行!!!!!'
-        },
-        include : User
-    }).then(user=>{
-        console.log(JSON.stringify(user, null, 4));
-    });
+users.create({
+    firstName : 'Samuel',
+    lastName : 'Jaskson'
+}).then(()=>{
+    console.log('new a user to users table')
 });
